facd30e9b92810e1f282fc8e17eb982016b1130f
Paulo Henrique <paulo.13.henrique800@gmail.com>
2023-11-30 15:16:08 -0300

    criando a classe Despesa

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
+class Despesa{
+
+	constructor(ano,mes,dia,tipo,descricao,valor){
+		this.ano=ano;
+		this.mes=mes;
+		this.dia=dia;
+		this.tipo=tipo;
+		this.descricao=descricao;
+		this.valor=valor;
+	}
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
 function cadastrarDespesa(){
 
 	let ano=document.getElementById('ano').value;
@@ -6,4 +30,9 @@ function cadastrarDespesa(){
 	let tipo=document.getElementById('tipo').value;
 	let descricao=document.getElementById('descricao').value;
 	let valor=document.getElementById('valor').value;
+
+
+	let despesa= new Despesa(ano,mes,dia,tipo,descricao,valor);
+
+	console.log(despesa);
 }
